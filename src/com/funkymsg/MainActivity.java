407a21c7d69e32ad216103d552c3578d9693dcf3
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-03-26 11:47:30 +0200

    progress bar

--- src/com/funkymsg/MainActivity.java
+++ src/com/funkymsg/MainActivity.java
@@ -10,40 +10,38 @@ import android.webkit.WebViewClient;
 import android.content.Intent;
 import android.webkit.URLUtil;
 
-public class MainActivity extends Activity {
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
+public class MainActivity extends Activity {@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-      final WebView webView = (WebView) findViewById(R.id.webView);
-      final WebSettings settings = webView.getSettings();
+		final WebView webView = (WebView) findViewById(R.id.webView);
+		final WebSettings settings = webView.getSettings();
 
-      settings.setJavaScriptEnabled(true);
+		settings.setJavaScriptEnabled(true);
 
-      final ProgressDialog progDailog = ProgressDialog.show(this, "Loading","Please wait...", true);
-        progDailog.setCancelable(false);
-      
-        webView.setWebViewClient(new WebViewClient() {
-         @Override
-         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-             if( URLUtil.isNetworkUrl(url) ) {
+		final ProgressDialog progDailog = ProgressDialog.show(this, "Loading", "Please wait...", true);
+		progDailog.setCancelable(false);
 
-                 progDailog.show();
-                 view.loadUrl(url);
-                }
-      else{
-          Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-          startActivity( intent );
-      }           
-            return true;
-         }
+		webView.setWebViewClient(new WebViewClient() {@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				if (URLUtil.isNetworkUrl(url)) {
 
-         @Override
-            public void onPageFinished(WebView view, final String url) {
-                progDailog.dismiss();
-            }
-     });
-      webView.loadUrl("https://funkymsg.com/");
-   }
+					progDailog.show();
+					view.loadUrl(url);
+				}
+				else {
+					Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+					startActivity(intent);
+				}
+				return true;
+			}
+
+			@Override
+			public void onPageFinished(WebView view, final String url) {
+				progDailog.dismiss();
+			}
+		});
+		webView.loadUrl("https://funkymsg.com/");
+	}
 }
