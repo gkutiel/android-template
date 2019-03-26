186042b493d3297bef970c2c4328356fe3a294cf
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-03-26 15:26:32 +0200

    adding touch

--- src/com/html5test/MainActivity.java
+++ src/com/html5test/MainActivity.java
@@ -5,43 +5,134 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.URLUtil;
+import android.view.View.OnTouchListener;
+import android.view.View;
+import android.view.GestureDetector;
+import android.content.Context;
+import android.view.GestureDetector.SimpleOnGestureListener;
 
-public class MainActivity extends Activity {@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		final WebView webView = (WebView) findViewById(R.id.webView);
-		final WebSettings settings = webView.getSettings();
+public class MainActivity extends Activity {
+	private final static String TAG = "GS";
 
-		settings.setJavaScriptEnabled(true);
-		settings.setDomStorageEnabled(true);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		final ProgressDialog progDailog = ProgressDialog.show(this, "Loading", "Please wait...", true);
-		progDailog.setCancelable(false);
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        final WebSettings settings = webView.getSettings();
 
-		webView.setWebViewClient(new WebViewClient() {@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				if (URLUtil.isNetworkUrl(url)) {
-					progDailog.show();
-					view.loadUrl(url);
-				}
-				else {
-					Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-					startActivity(intent);
-				}
-				return true;
-			}
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
 
-			@Override
-			public void onPageFinished(WebView view, final String url) {
-				progDailog.dismiss();
-			}
-		});
-		webView.loadUrl("https://html5test.com/");
-	}
+        final ProgressDialog progDailog = ProgressDialog.show(this, "Loading", "Please wait...", true);
+        progDailog.setCancelable(false);
+
+        webView.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeDown() {
+                Log.e(TAG, "onSwipeDown");
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                Log.e(TAG, "onSwipeLeft");
+            }
+
+            @Override
+            public void onSwipeUp() {
+                Log.e(TAG, "onSwipeUp");
+            }
+
+            @Override
+            public void onSwipeRight() {
+                Log.e(TAG, "onSwipeRight");
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (URLUtil.isNetworkUrl(url)) {
+                    progDailog.show();
+                    view.loadUrl(url);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                }
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, final String url) {
+                progDailog.dismiss();
+            }
+        });
+        webView.loadUrl("https://html5test.com/");
+    }
+}
+
+class OnSwipeTouchListener implements OnTouchListener {
+
+    private GestureDetector gestureDetector;
+
+    public OnSwipeTouchListener(Context c) {
+        gestureDetector = new GestureDetector(c, new GestureListener());
+    }
+
+    public boolean onTouch(final View view, final MotionEvent motionEvent) {
+        return gestureDetector.onTouchEvent(motionEvent);
+    }
+
+    private final class GestureListener extends SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                    }
+                } else {
+                    if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeDown();
+                        } else {
+                            onSwipeUp();
+                        }
+                    }
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    public void onSwipeRight() {}
+    public void onSwipeLeft() {}
+    public void onSwipeUp() {}
+    public void onSwipeDown() {}
 }
